eda158e3d9e2b0fead11d8104ca6b498b2b59a9f
Glufish <glufishh@gmail.com>
2025-09-07 03:29:13 -0700

    Update Slot_machine.py

--- Slot_machine.py
+++ Slot_machine.py
@@ -1,4 +1,6 @@
-#To randomise the number
+#Slot machine system
+#6 symbol:"cherry",  "lemon","watermelon", "grape", "apple","lucky7"
+#both lucky7 = 500, both same fruits = 200, 2 same fruits = 70, both different = -50
 import random
 
 
@@ -9,46 +11,87 @@ import random
 #-----------------------tool section------------------------
 
 #1.generate a random number variable
-def randomise_system( number ) :
-    number = random.randint ( 1 , 3 )
-    return number
+def randomise_system( reel1, reel2, reel3 ) :
+    reel1, reel2 , reel3 = random.randint ( 1 , 6 ) #random number between 1 to 6 for each reel
+    return reel1, reel2 , reel3 #return 3 reels by number
 
+#2.covert number to the responding symbol
+def number_to_symbol( reel1, reel2, reel3 ) :
+    #The way to use rule = { condition : result }
+    SymCovert = {   
+         1 : "cherry",  
+         2 : "lemon",
+         3 : "watermelon",
+         4 : "grape",
+         5 : "apple",
+         6 : "lucky7",
+        }
+
+    #covert each num to the symbol 
+    reel1 = SymCovert.get ( reel1 ) 
+    reel2 = SymCovert.get ( reel2 )
+    reel3 = SymCovert.get ( reel3 )
+
+    return reel1, reel2, reel3 #return 3 reels by syumbol
     
-    
+#3. update amount of the token
+def update_token( token, result ) :
+    token = token + result 
+    return token 
+
 #----------------------function---------------------------
 
-#game rule
-def game_rule() :
-    rules = {
+#check result
+def check_result( result, reel1, reel2, reel3) :
+    #Jackpot 7
+    if reel1 == reel2 == reel3 == 6 :
+        result = 500
 
-        }
+    #3 same fruits
+    elif reel1 == reel2 == reel3 :
+        result = 200
+
+    #2 same fruits
+    elif reel1 == reel2 or reel1 == reel3 or reel2 == reel3 :
+        result = 70
+
+    #both different
+    else :
+        result = -50
+
+    return result
 
 #-----------------------ui section------------------------
 
 #main menu ui
-def main_menu ( ) :
-    print("Guess the number:3")
-    print("Made by Fishyyy gluuu")
-    
+def main_menu():
+    print
+
 
      
 
 #----------------------main function-----------------------
+#have to put the token number outside the loop to prevent reset 
+token = 1000
+while True: #only when user exit then the application ended( with break )
+    main_menu() #show to main menu(waiting shrimpy)
 
-number = 0 #random number
-inputNumber = 0 #to let user input
-counter = 0 #to count the attempt of guess
-fishBool = False #bool to fix the bug wuwuwu
-main_menu() #ui 1
-while True:
-        
-        number = randomise_system( number ) #tool 1
-        inputNumber = number_range( inputNumber ) #tool 3     
-        fishBool, counter = compare_number( inputNumber, number, counter ) #tool 2
-        if fishBool :
-            break
+    reel1 , reel2 , reel3 = 0 #initialise the both reel
+    reel1 , reel2 , reel3 = randomise_system( reel1, reel2, reel3 ) #tool 1, randomise both reel number
 
-#application ended
+    result = 0 #initialise the result amount
+    result = check_result( result, reel1, reel2, reel3) #function 1, to get the result by the reel number
+
+    token = update_token( token, result )
+
+    
+
+
+
+
+
+
+
 
 
 
